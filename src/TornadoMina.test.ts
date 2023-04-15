ee4e129c4077a31d5076dbb8aa5521d488025148
Hyunggyu Jang <murasakipurplez5@gmail.com>
2023-04-16 00:47:22 +0900

    Implement TornadoMina & setup test

--- src/TornadoMina.test.ts
+++ src/TornadoMina.test.ts
@@ -1,7 +1,58 @@
-// import { TornadoMina } from './TornadoMina';
+import {
+  AccountUpdate,
+  MerkleMap,
+  Mina,
+  PrivateKey,
+  PublicKey,
+  isReady,
+  shutdown,
+} from 'snarkyjs';
+import { TornadoMina } from './TornadoMina';
 
 describe('TornadoMina.js', () => {
-  describe('TornadoMina()', () => {
-    it.todo('should be correct');
+  let zkApp: TornadoMina,
+    zkAppPrivateKey: PrivateKey,
+    zkAppAddress: PublicKey,
+    sender: PublicKey,
+    senderKey: PrivateKey,
+    commitmentMap: MerkleMap,
+    nullifierHashesMap: MerkleMap;
+
+  beforeEach(async () => {
+    await isReady;
+    commitmentMap = new MerkleMap();
+    nullifierHashesMap = new MerkleMap();
+    const Local = Mina.LocalBlockchain({ proofsEnabled: false });
+    Mina.setActiveInstance(Local);
+    sender = Local.testAccounts[0].publicKey;
+    senderKey = Local.testAccounts[0].privateKey;
+    Local.testAccounts[0];
+    zkAppPrivateKey = PrivateKey.random();
+    zkAppAddress = zkAppPrivateKey.toPublicKey();
+    zkApp = new TornadoMina(zkAppAddress);
+
+    const deployTxn = await Mina.transaction(sender, () => {
+      AccountUpdate.fundNewAccount(sender);
+      zkApp.deploy();
+    });
+    await deployTxn.prove();
+    await deployTxn.sign([senderKey, zkAppPrivateKey]).send();
+
+    const initTxn = await Mina.transaction(sender, () => {
+      zkApp.initState(commitmentMap.getRoot(), nullifierHashesMap.getRoot());
+    });
+    await initTxn.prove();
+    await initTxn.sign([senderKey]).send();
+  });
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  it('can deposit', async () => {});
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  it('should fail on depositing already deposit commiment', async () => {});
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  it('can withdraw', async () => {});
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  it('should fail on withdrawing already withdrawn commitment', async () => {});
+  afterAll(() => {
+    setTimeout(shutdown, 0);
   });
 });
--- src/TornadoMina.ts
+++ src/TornadoMina.ts
@@ -1,30 +1,77 @@
 import {
   Field,
-  MerkleTree,
-  MerkleMap,
-  Poseidon,
   SmartContract,
   State,
   method,
   state,
+  MerkleMapWitness,
+  AccountUpdate,
+  UInt64,
+  Poseidon,
 } from 'snarkyjs';
 
-const denomination = Field(10);
-
 export class TornadoMina extends SmartContract {
-  @state(Field) commitments = State<MerkleTree>();
-  @state(Field) nullifierHashes = State<MerkleMap>();
+  @state(Field) commitmentsRoot = State<Field>();
+  @state(Field) nullifierHashesRoot = State<Field>();
 
-  @method deposit(commitment: Field) {
-    // assert msg.value == denomination
-    // update commitment MerkleTree
+  @method initState(
+    initialCommitmentsRoot: Field,
+    initialNullifierHashesRoot: Field
+  ) {
+    this.commitmentsRoot.set(initialCommitmentsRoot);
+    this.nullifierHashesRoot.set(initialNullifierHashesRoot);
   }
 
-  @method withdraw(nullifier: Field, nonce: Field) {
+  @method deposit(commitment: Field, commitmentWitness: MerkleMapWitness) {
+    const initialRoot = this.commitmentsRoot.get();
+    this.commitmentsRoot.assertEquals(initialRoot);
+
+    // commitment = Poseidon(nullifier + nonce)
+    const [rootBefore, key] = commitmentWitness.computeRootAndKey(Field(0));
+    rootBefore.assertEquals(initialRoot);
+
+    key.assertEquals(commitment);
+
+    // compute the root after incrementing
+    const [rootAfter] = commitmentWitness.computeRootAndKey(Field(1));
+
+    this.commitmentsRoot.set(rootAfter);
+
+    const payerUpdate = AccountUpdate.create(this.sender);
+    payerUpdate.send({ to: this.address, amount: UInt64.from(1000000) });
+  }
+
+  @method withdraw(
+    nullifier: Field,
+    nonce: Field,
+    nullifierWitness: MerkleMapWitness,
+    commitmentWitness: MerkleMapWitness
+  ) {
     // assert nullifierHash not yet used
+    const initialRoot = this.nullifierHashesRoot.get();
+    this.nullifierHashesRoot.assertEquals(initialRoot);
+
+    const [rootBefore, key] = nullifierWitness.computeRootAndKey(Field(0));
+    rootBefore.assertEquals(initialRoot);
+
+    const nullifierHash = Poseidon.hash([nullifier]);
+    key.assertEquals(nullifierHash);
+
     // assert commitment = Poseidon(nullifier + nonce)
+    const expectedCommitment = Poseidon.hash([nullifier, nonce]);
+    const [commitmentRootBefore, commitment] =
+      commitmentWitness.computeRootAndKey(Field(1));
     // assert commitment is included in MerkleTree
+    const commitmentRoot = this.commitmentsRoot.get();
+    this.commitmentsRoot.assertEquals(initialRoot);
+    commitmentRootBefore.assertEquals(commitmentRoot);
+    commitment.assertEquals(expectedCommitment);
+
     // set nullifierHashes(nullifierHash) as True
+    const [rootAfter] = nullifierWitness.computeRootAndKey(Field(1));
+    this.nullifierHashesRoot.set(rootAfter);
+
     // transfer denomination MINA
+    this.send({ to: this.sender, amount: UInt64.from(1000000) });
   }
 }
