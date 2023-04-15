c5b5204b1ccbe60572f216d1bcbdc15d17745d6d
Hyunggyu Jang <murasakipurplez5@gmail.com>
2023-04-15 21:07:01 +0900

    Implement test

--- src/IncrementSecret.test.ts
+++ src/IncrementSecret.test.ts
@@ -1,4 +1,13 @@
-import { Mina, PrivateKey, PublicKey, isReady } from 'snarkyjs';
+import {
+  AccountUpdate,
+  Field,
+  Mina,
+  Poseidon,
+  PrivateKey,
+  PublicKey,
+  isReady,
+  shutdown,
+} from 'snarkyjs';
 import { IncrementSecret } from './IncrementSecret';
 
 describe('IncrementSecret.js', () => {
@@ -12,5 +21,45 @@ describe('IncrementSecret.js', () => {
     await isReady;
     let Local = Mina.LocalBlockchain({ proofsEnabled: false });
     Mina.setActiveInstance(Local);
+    sender = Local.testAccounts[0].publicKey;
+    senderKey = Local.testAccounts[0].privateKey;
+    zkAppPrivateKey = PrivateKey.random();
+    zkAppAddress = zkAppPrivateKey.toPublicKey();
+    zkApp = new IncrementSecret(zkAppAddress);
+  });
+  afterAll(() => {
+    setTimeout(shutdown, 0);
+  });
+
+  it('can deploy', async () => {
+    await deploy(zkApp, zkAppPrivateKey, sender, senderKey);
+  });
+
+  it('can deploy and initiate', async () => {
+    await deploy(zkApp, zkAppPrivateKey, sender, senderKey);
+    const salt = Field.random();
+    const secret = Field(750);
+    const tx = await Mina.transaction(sender, () => {
+      zkApp.initState(salt, secret);
+    });
+    await tx.prove();
+    await tx.sign([senderKey]).send();
+    const retrievedCommitment = zkApp.x.get();
+    expect(retrievedCommitment).toStrictEqual(Poseidon.hash([salt, secret]));
   });
 });
+
+async function deploy(
+  zkApp: IncrementSecret,
+  zkAppPrivateKey: PrivateKey,
+  sender: PublicKey,
+  senderKey: PrivateKey
+) {
+  let tx = await Mina.transaction(sender, () => {
+    AccountUpdate.fundNewAccount(sender);
+    zkApp.deploy();
+  });
+  await tx.prove();
+  // this tx needs .sign(), because `deploy()` adds an account update that requires signature authorization
+  await tx.sign([zkAppPrivateKey, senderKey]).send();
+}
